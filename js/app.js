9fbac590abe0ab135b4e7111757c5c4c25a24942
Uzor Mercy <mercy4hope@gmail.com>
2019-08-08 22:51:53 +0000

    landing page without the footer

---
+++ js/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var project_carousel = $('#our-project');
+project_carousel.owlCarousel({
+	loop:true,
+        nav:true,
+        autoplay:true,
+        dots:false,
+        navText: ["<i class='icon icon-chevron-left'></i>","<i class='icon icon-chevron-right'></i>"],
+        responsive:{
+            0: {
+                items: 1
+            },
+            481: {
+                items: 2
+            },
+            700: {
+                items: 3
+            },
+            992: {
+                items: 5
+            }
+        }
+    });
+
+let client_carousel = $('#our-client-slide');
+client_carousel.owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        481:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
