539629576b83febed71ebc70a437edd733ec4b8f
Uzor Mercy <mercy4hope@gmail.com>
2019-10-24 17:02:28 +0100

    added the slider to the banner section

--- js/app.js
+++ js/app.js
@@ -55,3 +55,48 @@ client_carousel.owlCarousel({
         }
     }
 })
+
+   // bootstrap animated slider
+   function bootstrapAnimatedLayer() {
+
+    /* Demo Scripts for Bootstrap Carousel and Animate.css article
+     * on SitePoint by Maria Antonietta Perna
+     */
+
+    //Function to animate slider captions 
+    function doAnimations(elems) {
+        //Cache the animationend event in a variable
+        var animEndEv = 'webkitAnimationEnd animationend';
+
+        elems.each(function() {
+            var $this = $(this),
+                $animationType = $this.data('animation');
+            $this.addClass($animationType).one(animEndEv, function() {
+                $this.removeClass($animationType);
+            });
+        });
+    }
+
+    //Variables on page load 
+    var $myCarousel = $('#minimal-bootstrap-carousel'),
+        $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+
+    //Initialize carousel 
+    $myCarousel.carousel({
+        interval: 7000
+    });
+
+    //Animate captions in first slide on page load 
+    doAnimations($firstAnimatingElems);
+
+    //Pause carousel  
+    $myCarousel.carousel('pause');
+
+
+    //Other slides to be animated on carousel slide event 
+    $myCarousel.on('slide.bs.carousel', function(e) {
+        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+        doAnimations($animatingElems);
+    });
+}
+bootstrapAnimatedLayer();
